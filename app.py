784a2a6dd27da5af17c5c7ae31f94704a42ac844
jorgemansillacriado <jarabers@gmail.com>
2020-04-28 15:02:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def main():
-    return "Welcome!"
-@app.route('how are you')
+    return "Welcome Jorge!"
+@app.route('how are you today')
 def hello():
     return 'I am good, how about you'
 if __name__ == "__main__":
